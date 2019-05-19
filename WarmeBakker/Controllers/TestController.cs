cc01785bfcff0cb49deaf28dccd9c781eac09e55
steven <nonkelchameau@gmail.com>
2019-05-19 21:37:05 +0200

    Picture

--- WarmeBakker/Controllers/TestController.cs
+++ WarmeBakker/Controllers/TestController.cs
@@ -113,7 +113,7 @@ namespace WarmeBakker.Controllers
         }
 
         // POST: Test/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        ///To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
